069e3bfa14f0656b9140c23c3e45258786c0e8ab
hostingjc <hosting@juandiaz.co.ve>
2020-07-13 19:34:32 +0000

    change request

--- public/index.php
+++ public/index.php
@@ -52,8 +52,14 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+    $request = App\Http\Request::capture()
 );
+
+
+/*
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);*/
 
 $response->send();
 
